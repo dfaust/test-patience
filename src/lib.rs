4165279b195a3dacf62038fd0b33e4a15343d24e
Daniel Faust <hessijames@gmail.com>
2017-01-28 18:06:21 +0100

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,22 @@
-//! `test-patience` is a utility to synchronize the startup of applications that are part of an integration test and the test itself.
+//! test-patience is a utility to synchronize the startup of applications that are part of an integration test and the test itself.
+//!
+//! When writing integration tests, it is often necessary to launch applications that are part of the test.
+//! This can take some time and if the test has to wait until the application is running, valuable time is lost when waiting for a fixed duration.
+//! Also waiting for a fixed duration can still lead to test failures without producing a clear error message.
+//! test-patience waits exactly until the starting application signals that it's ready or a specified timeout period has passed.
+//!
+//! # Using test-patience
 //!
 //! The test has to create an instance of the `Server` struct, which starts a TCP server and returns a port number.
 //! That port number needs to be sent to the application that is needed to execute the test.
 //! This could be done using an environment variable, an argument or a configuration file.
 //! After the start of the application has been initiated, the `wait` method needs to be called.
-//! It blocks the currently running thread until either the starting application has signaled its successful start or the `timeout` period has expired.
+//! It blocks the currently running thread until either the starting application has signaled its successful start or the `timeout` period has passed.
 //!
 //! When the application is ready, it has to create an instance of the `Client` struct and call the `notify` method with the correct port number.
 //! After that the thread of the test continues executing.
+//!
+//! In order to disable startup notifications in release builds, use `cfg!(debug_assertions)` (see [conditional compilation](https://doc.rust-lang.org/reference.html#conditional-compilation)).
 //!
 //! # Examples
 //!
