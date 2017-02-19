062b506bd3cadfaacca3fe926e590963c6cfab77
Daniel Faust <hessijames@gmail.com>
2017-02-19 18:03:55 +0100

    Improve tests

--- tests/test_patience.rs
+++ tests/test_patience.rs
@@ -9,74 +9,89 @@ fn mock_client(port: u16, sleep: Duration) {
     let _ = test_patience::Client::notify(port); // ignore errors when testing timeout
 }
 
-#[test]
-fn wait_for_client_0() {
-    let server = test_patience::Server::new().expect("failed to create test-patience server");
-    let port = server.port().expect("failed to get test-patience server port");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    thread::spawn(move || {
-        mock_client(port, Duration::from_secs(0));
-    });
+    #[macro_export]
+    macro_rules! assert_close {
+        ($left:expr, $right:expr) => ({
+            let diff = $left - $right;
+            if diff.as_secs() > 0 || diff.subsec_nanos() > 10_000_000 {
+                panic!("assertion failed: `(time difference smaller than 10 ms)`, (diff: `{:?}`)", diff)
+            }
+        })
+    }
 
-    let result = server.wait(Duration::from_secs(5));
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap().as_secs(), 0);
-}
+    #[test]
+    fn wait_for_client_0() {
+        let server = test_patience::Server::new().expect("failed to create test-patience server");
+        let port = server.port().expect("failed to get test-patience server port");
 
-#[test]
-fn wait_for_client_1() {
-    let server = test_patience::Server::new().expect("failed to create test-patience server");
-    let port = server.port().expect("failed to get test-patience server port");
+        thread::spawn(move || {
+            mock_client(port, Duration::from_secs(0));
+        });
 
-    thread::spawn(move || {
-        mock_client(port, Duration::from_secs(1));
-    });
+        let wait_duration = server.wait(Duration::from_secs(5)).expect("failed to wait");
 
-    let result = server.wait(Duration::from_secs(5));
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap().as_secs(), 1);
-}
+        assert_close!(wait_duration, Duration::from_secs(0));
+    }
 
-#[test]
-fn wait_for_client_2() {
-    let server = test_patience::Server::new().expect("failed to create test-patience server");
-    let port = server.port().expect("failed to get test-patience server port");
+    #[test]
+    fn wait_for_client_1() {
+        let server = test_patience::Server::new().expect("failed to create test-patience server");
+        let port = server.port().expect("failed to get test-patience server port");
 
-    thread::spawn(move || {
-        mock_client(port, Duration::from_secs(2));
-    });
+        thread::spawn(move || {
+            mock_client(port, Duration::from_secs(1));
+        });
 
-    let result = server.wait(Duration::from_secs(5));
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap().as_secs(), 2);
-}
+        let wait_duration = server.wait(Duration::from_secs(5)).expect("failed to wait");
 
-#[test]
-fn wait_for_client_buffered() {
-    let server = test_patience::Server::new().expect("failed to create test-patience server");
-    let port = server.port().expect("failed to get test-patience server port");
+        assert_close!(wait_duration, Duration::from_secs(1));
+    }
 
-    thread::spawn(move || {
-        mock_client(port, Duration::from_secs(0));
-    });
+    #[test]
+    fn wait_for_client_2() {
+        let server = test_patience::Server::new().expect("failed to create test-patience server");
+        let port = server.port().expect("failed to get test-patience server port");
 
-    thread::sleep(Duration::from_secs(1));
+        thread::spawn(move || {
+            mock_client(port, Duration::from_secs(2));
+        });
 
-    let result = server.wait(Duration::from_secs(5));
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap().as_secs(), 0);
-}
+        let wait_duration = server.wait(Duration::from_secs(5)).expect("failed to wait");
 
-#[test]
-fn wait_for_client_timeout() {
-    let server = test_patience::Server::new().expect("failed to create test-patience server");
-    let port = server.port().expect("failed to get test-patience server port");
+        assert_close!(wait_duration, Duration::from_secs(2));
+    }
 
-    thread::spawn(move || {
-        mock_client(port, Duration::from_secs(2));
-    });
+    #[test]
+    fn wait_for_client_buffered() {
+        let server = test_patience::Server::new().expect("failed to create test-patience server");
+        let port = server.port().expect("failed to get test-patience server port");
 
-    let result = server.wait(Duration::from_secs(1));
-    assert!(result.is_err());
-    assert_eq!(result.unwrap_err().kind(), io::ErrorKind::TimedOut);
+        thread::spawn(move || {
+            mock_client(port, Duration::from_secs(0));
+        });
+
+        thread::sleep(Duration::from_secs(1));
+
+        let wait_duration = server.wait(Duration::from_secs(5)).expect("failed to wait");
+
+        assert_close!(wait_duration, Duration::from_secs(0));
+    }
+
+    #[test]
+    fn wait_for_client_timeout() {
+        let server = test_patience::Server::new().expect("failed to create test-patience server");
+        let port = server.port().expect("failed to get test-patience server port");
+
+        thread::spawn(move || {
+            mock_client(port, Duration::from_secs(2));
+        });
+
+        let result = server.wait(Duration::from_secs(1));
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().kind(), io::ErrorKind::TimedOut);
+    }
 }
